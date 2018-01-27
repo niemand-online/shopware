86b9e6ae073544ea4c70a522a50fa7ada6a46aeb
Joshua Behrens <joshua.behrens@niemand.online>
2018-01-27 22:39:42 +0100

    Implemented CompletionAwareInterface for PluginUninstallCommand

--- engine/Shopware/Commands/PluginUninstallCommand.php
+++ engine/Shopware/Commands/PluginUninstallCommand.php
@@ -25,6 +25,10 @@
 namespace Shopware\Commands;
 
 use Shopware\Bundle\PluginInstallerBundle\Service\InstallerService;
+use Shopware\Components\Model\ModelRepository;
+use Shopware\Models\Plugin\Plugin;
+use Stecman\Component\Symfony\Console\BashCompletion\Completion\CompletionAwareInterface;
+use Stecman\Component\Symfony\Console\BashCompletion\CompletionContext;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -36,7 +40,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * @copyright Copyright (c) shopware AG (http://www.shopware.de)
  */
-class PluginUninstallCommand extends ShopwareCommand
+class PluginUninstallCommand extends ShopwareCommand implements CompletionAwareInterface
 {
     /**
      * {@inheritdoc}
@@ -91,5 +95,33 @@ EOF
         $pluginManager->uninstallPlugin($plugin, $removeData);
 
         $output->writeln(sprintf('Plugin %s has been uninstalled successfully.', $pluginName));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function completeOptionValues($optionName, CompletionContext $context)
+    {
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function completeArgumentValues($argumentName, CompletionContext $context)
+    {
+        if ($argumentName === 'plugin') {
+            /** @var ModelRepository $repository */
+            $repository = $this->getContainer()->get('models')->getRepository(Plugin::class);
+            $queryBuilder = $repository->createQueryBuilder('plugin');
+            $result = $queryBuilder->andWhere($queryBuilder->expr()->eq('plugin.capabilityEnable', 'true'))
+                ->andWhere($queryBuilder->expr()->isNotNull('plugin.installed'))
+                ->select(['plugin.name'])
+                ->getQuery()
+                ->getArrayResult();
+            return array_column($result, 'name');
+        }
+
+        return false;
     }
 }
