f1ff910eef16d64a5222e3b7ceb8f03e128dc94b
Joshua Behrens <joshua.behrens@niemand.online>
2018-01-28 01:22:48 +0100

    Implemented CompletionAwareInterface for SnippetsToDbCommand

--- engine/Shopware/Commands/SnippetsToDbCommand.php
+++ engine/Shopware/Commands/SnippetsToDbCommand.php
@@ -27,6 +27,9 @@ namespace Shopware\Commands;
 use Shopware\Components\Snippet\DatabaseHandler;
 use Shopware\Kernel;
 use Shopware\Models\Plugin\Plugin;
+use Stecman\Component\Symfony\Console\BashCompletion\Completion\CompletionAwareInterface;
+use Stecman\Component\Symfony\Console\BashCompletion\Completion\ShellPathCompletion;
+use Stecman\Component\Symfony\Console\BashCompletion\CompletionContext;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -36,7 +39,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * @copyright Copyright (c) shopware AG (http://www.shopware.de)
  */
-class SnippetsToDbCommand extends ShopwareCommand
+class SnippetsToDbCommand extends ShopwareCommand implements CompletionAwareInterface
 {
     /**
      * {@inheritdoc}
@@ -128,5 +131,25 @@ class SnippetsToDbCommand extends ShopwareCommand
         }
 
         return $plugins[$pluginName];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function completeOptionValues($optionName, CompletionContext $context)
+    {
+        if ($optionName === 'source') {
+            exit(ShellPathCompletion::PATH_COMPLETION_EXIT_CODE);
+        }
+
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function completeArgumentValues($argumentName, CompletionContext $context)
+    {
+        return false;
     }
 }
