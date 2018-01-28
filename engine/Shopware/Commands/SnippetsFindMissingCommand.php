548c31cae5f5afebdda11dd663873cebbf6fbf50
Joshua Behrens <joshua.behrens@niemand.online>
2018-01-28 01:22:12 +0100

    Implemented CompletionAwareInterface for SnippetsFindMissingCommand

--- engine/Shopware/Commands/SnippetsFindMissingCommand.php
+++ engine/Shopware/Commands/SnippetsFindMissingCommand.php
@@ -24,6 +24,11 @@
 
 namespace Shopware\Commands;
 
+use Shopware\Components\Model\ModelRepository;
+use Shopware\Models\Shop\Locale;
+use Stecman\Component\Symfony\Console\BashCompletion\Completion\CompletionAwareInterface;
+use Stecman\Component\Symfony\Console\BashCompletion\Completion\ShellPathCompletion;
+use Stecman\Component\Symfony\Console\BashCompletion\CompletionContext;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -34,7 +39,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * @copyright Copyright (c) shopware AG (http://www.shopware.de)
  */
-class SnippetsFindMissingCommand extends ShopwareCommand
+class SnippetsFindMissingCommand extends ShopwareCommand implements CompletionAwareInterface
 {
     /**
      * {@inheritdoc}
@@ -156,5 +161,59 @@ class SnippetsFindMissingCommand extends ShopwareCommand
         foreach ($data as $namespace) {
             $outputAdapter->write($namespace, true);
         }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function completeOptionValues($optionName, CompletionContext $context)
+    {
+        if ($optionName === 'target') {
+            exit(ShellPathCompletion::PATH_COMPLETION_EXIT_CODE);
+        }
+
+        if ($optionName === 'fallback') {
+            /** @var ModelRepository $localeRepository */
+            $localeRepository = $this->getContainer()->get('models')->getRepository(Locale::class);
+            $queryBuilder = $localeRepository->createQueryBuilder('locale');
+
+            if (strlen($context->getCurrentWord())) {
+                $queryBuilder->andWhere($queryBuilder->expr()->like('locale.locale', ':search'))
+                    ->setParameter('search', addcslashes($context->getCurrentWord(), '_%').'%');
+            }
+
+            $result = $queryBuilder->select(['locale.locale'])
+                ->getQuery()
+                ->getArrayResult();
+
+            return array_diff(array_column($result, 'locale'), [$context->getWordAtIndex(2)]);
+        }
+
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function completeArgumentValues($argumentName, CompletionContext $context)
+    {
+        if ($argumentName === 'locale') {
+            /** @var ModelRepository $localeRepository */
+            $localeRepository = $this->getContainer()->get('models')->getRepository(Locale::class);
+            $queryBuilder = $localeRepository->createQueryBuilder('locale');
+
+            if (strlen($context->getCurrentWord())) {
+                $queryBuilder->andWhere($queryBuilder->expr()->like('locale.locale', ':search'))
+                    ->setParameter('search', addcslashes($context->getCurrentWord(), '_%').'%');
+            }
+
+            $result = $queryBuilder->select(['locale.locale'])
+                ->getQuery()
+                ->getArrayResult();
+
+            return array_column($result, 'locale');
+        }
+
+        return false;
     }
 }
