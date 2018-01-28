aae2ca32c7f604848132a1fd19b362355a848b74
Joshua Behrens <joshua.behrens@niemand.online>
2018-01-28 01:38:16 +0100

    Implemented CompletionAwareInterface for StoreListUpdatesCommand

--- engine/Shopware/Commands/StoreListUpdatesCommand.php
+++ engine/Shopware/Commands/StoreListUpdatesCommand.php
@@ -26,6 +26,8 @@ namespace Shopware\Commands;
 
 use Shopware\Bundle\PluginInstallerBundle\Context\UpdateListingRequest;
 use Shopware\Bundle\PluginInstallerBundle\Struct\UpdateResultStruct;
+use Stecman\Component\Symfony\Console\BashCompletion\Completion\CompletionAwareInterface;
+use Stecman\Component\Symfony\Console\BashCompletion\CompletionContext;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -34,7 +36,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * @copyright Copyright (c) shopware AG (http://www.shopware.de)
  */
-class StoreListUpdatesCommand extends StoreCommand
+class StoreListUpdatesCommand extends StoreCommand implements CompletionAwareInterface
 {
     /**
      * {@inheritdoc}
@@ -83,5 +85,25 @@ class StoreListUpdatesCommand extends StoreCommand
               ->setRows($result);
 
         $table->render($output);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function completeOptionValues($optionName, CompletionContext $context)
+    {
+        if ($optionName === 'shopware-version') {
+            return [\Shopware::VERSION];
+        }
+
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function completeArgumentValues($argumentName, CompletionContext $context)
+    {
+        return false;
     }
 }
