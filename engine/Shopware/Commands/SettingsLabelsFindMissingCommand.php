35edf71daba9f952becc581377377554731fcdd7
Joshua Behrens <joshua.behrens@niemand.online>
2018-01-28 01:21:24 +0100

    Implemented CompletionAwareInterface for SettingsLabelsFindMissingCommand

--- engine/Shopware/Commands/SettingsLabelsFindMissingCommand.php
+++ engine/Shopware/Commands/SettingsLabelsFindMissingCommand.php
@@ -24,7 +24,11 @@
 
 namespace Shopware\Commands;
 
+use Shopware\Components\Model\ModelRepository;
 use Shopware\Models\Shop\Locale;
+use Stecman\Component\Symfony\Console\BashCompletion\Completion\CompletionAwareInterface;
+use Stecman\Component\Symfony\Console\BashCompletion\Completion\ShellPathCompletion;
+use Stecman\Component\Symfony\Console\BashCompletion\CompletionContext;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -35,7 +39,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * @copyright Copyright (c) shopware AG (http://www.shopware.de)
  */
-class SettingsLabelsFindMissingCommand extends ShopwareCommand
+class SettingsLabelsFindMissingCommand extends ShopwareCommand implements CompletionAwareInterface
 {
     /**
      * {@inheritdoc}
@@ -168,5 +172,42 @@ class SettingsLabelsFindMissingCommand extends ShopwareCommand
             $output->writeln('<info>Writing to ' . $elementLabelFilePath . '</info>');
             file_put_contents($elementLabelFilePath, '<?php return ' . var_export($missingElementLabels, true) . ';');
         }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function completeOptionValues($optionName, CompletionContext $context)
+    {
+        if ($optionName === 'target') {
+            exit(ShellPathCompletion::PATH_COMPLETION_EXIT_CODE);
+        }
+
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function completeArgumentValues($argumentName, CompletionContext $context)
+    {
+        if ($argumentName === 'locale') {
+            /** @var ModelRepository $localeRepository */
+            $localeRepository = $this->getContainer()->get('models')->getRepository(Locale::class);
+            $queryBuilder = $localeRepository->createQueryBuilder('locale');
+
+            if (strlen($context->getCurrentWord())) {
+                $queryBuilder->andWhere($queryBuilder->expr()->like('locale.locale', ':search'))
+                    ->setParameter('search', addcslashes($context->getCurrentWord(), '_%').'%');
+            }
+
+            $result = $queryBuilder->select(['locale.locale'])
+                ->getQuery()
+                ->getArrayResult();
+
+            return array_column($result, 'locale');
+        }
+
+        return false;
     }
 }
